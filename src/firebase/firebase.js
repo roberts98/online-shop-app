31b87a018087357006429463a5de36737011ea8e
roberts98 <robertsidzinka@interia.pl>
2018-12-10 22:06:01 +0100

    firebase fix (2)

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: 'AIzaSyDDvkYqcR4GuceBACqjTPw-p2LRbsnOpJs',
+  authDomain: 'online-shop-8da9c.firebaseapp.com',
+  databaseURL: 'https://online-shop-8da9c.firebaseio.com',
+  projectId: 'online-shop-8da9c',
+  storageBucket: 'online-shop-8da9c.appspot.com',
+  messagingSenderId: '41467605275'
 };
 
 firebase.initializeApp(config);
